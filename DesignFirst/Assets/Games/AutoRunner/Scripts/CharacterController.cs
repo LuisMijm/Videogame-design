661cb1fef5d66e23968806fb2041e7c8a92950ac
Luis Miguel Jimenez Muñoz <jimenezmu@esat-alumni.com>
2023-11-17 00:39:49 +0100

    some fixes in the autorrunner about animations, particles and other minor changes

--- DesignFirst/Assets/Games/AutoRunner/Scripts/CharacterController.cs
+++ DesignFirst/Assets/Games/AutoRunner/Scripts/CharacterController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class CharacterController : MonoBehaviour
 {
@@ -17,6 +18,7 @@ public class CharacterController : MonoBehaviour
     public ParticleSystem particleSystem_;
 
     public Animator anim_;
+    private string currentState_;
 
     Vector3 spawmPoint_;
     public GameObject deathPlane_;
@@ -25,6 +27,8 @@ public class CharacterController : MonoBehaviour
 
     public GameObject floorObj_;
     public FloorController floorControl_;
+
+    public bool isGrounded_;
     
     // Start is called before the first frame update
     void Start()
@@ -33,14 +37,14 @@ public class CharacterController : MonoBehaviour
         forwardSpeed_ = 20.0f;
         spawmPoint_ = new Vector3(-1.2f, 8.54f, -785.41f);
 
-        jumpForce_ = 5.0f;
+        // jumpForce_ = 5.0f;
         rb_ = GetComponent<Rigidbody>();
         FallingCastDistance_ = 1.0f;
         coins_ = 0;
         distance_ = 0;
         floorObj_ = GameObject.Find("FloorObj");
         floorControl_ = floorObj_.GetComponent<FloorController>();
-        anim_ = GetComponent<Animator>();
+        // anim_ = GetComponent<Animator>();
 
     }
 
@@ -57,7 +61,8 @@ public class CharacterController : MonoBehaviour
         newVelocity.y = verticalVelocity;
 
         rb_.velocity = newVelocity;
-
+        
+        distance_++;
     }
 
     public void ResetPosition()
@@ -65,31 +70,43 @@ public class CharacterController : MonoBehaviour
         this.transform.position = spawmPoint_;
         // coins_ = 0;
         distance_ = 0;
+        this.transform.rotation = Quaternion.Euler(0,0,0);
     }
 
     void ActivateParticles()
     {
-        if (particleSystem_.isPaused)
-        {   
+        // if (particleSystem_.isPaused)
+        // {
+            Debug.Log("Reanudando partículas");
             particleSystem_.Play();
-        }
+            Debug.Log("¿Partículas activas?: " + particleSystem_.isPlaying);
+
+        // }
     }
 
     void DeactivateParticleSystem()
     {
         if (particleSystem_.isPlaying)
         {
+            Debug.Log("Deteniendo partículas");
             particleSystem_.Stop();
+            // particleSystem_.Clear();
+            // particleSystem_.
+            Debug.Log("¿Partículas detenidas?: " + particleSystem_.isPlaying);
         }
+    }
+
+    void ChangeAnimationState(string newState)
+    {
+        if (currentState_ == newState) return;
+        anim_.Play(newState);
+        currentState_ = newState;
     }
 
 
     // Update is called once per frame
     void Update()
     {
-
-        Run();
-
         // Movimiento
 
         if(Input.GetKey(KeyCode.W))
@@ -149,40 +166,77 @@ public class CharacterController : MonoBehaviour
                                                                   deathPlane_.transform.position.y,
                                                                   this.transform.position.z),
                                                       1.0f);
-    }
 
-    private void FixedUpdate()
-    {
-        Debug.Log("ground: " + anim_.GetBool("ground"));
-        if (Physics.Raycast(this.transform.position, Vector3.down, FallingCastDistance_, LayerMask.GetMask("Floor")))  
+
+        // Debug.Log("ground: " + anim_.GetBool("ground"));
+        if (Physics.Raycast(this.transform.position, Vector3.down, FallingCastDistance_, LayerMask.GetMask("Floor")))
         {
             // Debug.Log("TouchingGround" + true);
-            anim_.SetBool("ground", true);
-        
+            // anim_.SetBool("ground", true);
+            
             //anim_.SetBool("falling_", false);
 
             if (Input.GetKey(KeyCode.Space))
             {
                 rb_.AddForce(Vector3.up * jumpForce_, ForceMode.Impulse);
-                anim_.SetBool("ground", false);
+                // anim_.SetBool("ground", false);
+                // anim_.Play("Jump");
+                ActivateParticles();
             }
-            mainCamera_.fieldOfView = Mathf.Lerp(mainCamera_.fieldOfView, 60, 0.1f);
+            ChangeAnimationState("Run");
+            isGrounded_ = true;
+            mainCamera_.fieldOfView = Mathf.Lerp(mainCamera_.fieldOfView, 60, 0.01f);
 
             DeactivateParticleSystem();
-        }  
+        }
         else
         {
-            //anim_.SetBool("falling_", true);
+            // anim_.SetBool("falling_", true);
             // anim_.SetBool("ground", false);
             // Debug.Log("TouchingGround" + false);
             //mainCamera_.fieldOfView = 80;
-            mainCamera_.fieldOfView = Mathf.Lerp(mainCamera_.fieldOfView, 80, 0.1f);
-
-            ActivateParticles();
+            mainCamera_.fieldOfView = Mathf.Lerp(mainCamera_.fieldOfView, 80, 0.01f);
+            ChangeAnimationState("Jump");
+            isGrounded_ = false;
         }
         Debug.DrawRay(this.transform.position, Vector3.down * FallingCastDistance_, Color.red);
 
-        distance_++;
+    }
+
+    private void FixedUpdate()
+    {
+        Run();
+
+        // Debug.Log("ground: " + anim_.GetBool("ground"));
+        // if (Physics.Raycast(this.transform.position, Vector3.down, FallingCastDistance_, LayerMask.GetMask("Floor")))  
+        // {
+        //     // Debug.Log("TouchingGround" + true);
+        //     anim_.SetBool("ground", true);
+
+        //     //anim_.SetBool("falling_", false);
+
+        //     if (Input.GetKey(KeyCode.Space))
+        //     {
+        //         rb_.AddForce(Vector3.up * jumpForce_, ForceMode.Impulse);
+        //         anim_.SetBool("ground", false);
+        //     }
+        //     mainCamera_.fieldOfView = Mathf.Lerp(mainCamera_.fieldOfView, 60, 0.1f);
+
+        //     DeactivateParticleSystem();
+        // }  
+        // else
+        // {
+        //     //anim_.SetBool("falling_", true);
+        //     // anim_.SetBool("ground", false);
+        //     // Debug.Log("TouchingGround" + false);
+        //     //mainCamera_.fieldOfView = 80;
+        //     mainCamera_.fieldOfView = Mathf.Lerp(mainCamera_.fieldOfView, 80, 0.1f);
+
+        //     ActivateParticles();
+        // }
+        // Debug.DrawRay(this.transform.position, Vector3.down * FallingCastDistance_, Color.red);
+
+        // distance_++;
 
     }
 
@@ -193,7 +247,6 @@ public class CharacterController : MonoBehaviour
             coins_++;
             other.gameObject.SetActive(false);
         }
-
         // Debug.Log(other.gameObject.tag);
 
         if("FloorSpawner" == other.gameObject.tag)
--- DesignFirst/Assets/Games/AutoRunner/Scripts/CoinController.cs
+++ DesignFirst/Assets/Games/AutoRunner/Scripts/CoinController.cs
@@ -6,21 +6,25 @@ public class CoinController : MonoBehaviour
 {
 
     float rotationSpeed_ = 90.0f * 2;
+    private Transform tr_; 
 
     void Start()
     {
         // transform.LookAt(0.0f, 0.0f, 90.0f);
-        transform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
-
+        // transform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
         // Quaternion.Euler(0.0f, 0.0f, 90.0f)
+
+        tr_ = GetComponentInParent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.back, rotationSpeed_ * Time.deltaTime);
+        transform.SetParent(null);
+        // transform.Rotate(0, rotationSpeed_ * Time.deltaTime, 0);
+        transform.Rotate(new Vector3(0.0f, rotationSpeed_ * Time.deltaTime, 0.0f), Space.World);
         // transform.localRotation = Quaternion.Euler(new Vector3(0, 45, 0));
-
         // transform.Rotate(Vector3.forward, rotationSpeed_ * Time.deltaTime);
+        transform.SetParent(tr_);
     }
 }
